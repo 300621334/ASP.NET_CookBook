ef7d8d61c3010a21dc5b2f628a8bbc54a34325fe
300621334 <shafiqu@my.centennialcollege.ca>
2016-10-31 01:04:26 -0400

    File downling ability added. Recipe page improved. Pop-up msg of sucessful download added.

--- recipes.aspx
+++ recipes.aspx
@@ -1,8 +1,9 @@
 ﻿<%@ Page MasterPageFile="MasterPage.master" Language="C#" AutoEventWireup="true" CodeFile="recipes.aspx.cs" Inherits="recipes" %>
-
+<%--MaintainScrollPositionOnPostback="true" indise page directive keeps scroll position BUT page flickers very badly--%>
 
 <asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server" ID="recipes">
-
+    <asp:Button ID="openDbFile" runat="server" Text="Open Database File" OnClick="openDbFile_Click" />
+    <br />
     ALL the  Recipes added so far:
 
     <%--<asp:DataGrid ID="displayRecipes" runat="server"></asp:DataGrid>--%>
@@ -13,12 +14,12 @@
             <%# "Recipe:" + Eval("recipeName") %>
 
             <%--EnableViewState="false" inside Literal would erase itself & showDetal btn doesn't show anything--%>
-            <asp:Literal ID="byLit" runat="server"        Text=<%# Eval("fromName")    +"<br />" %>        Visible="false"     />
-            <asp:Literal ID="timeLit" runat="server"      Text=<%# Eval("cookingTime") +"<br />" %>   Visible="false"     />
-            <asp:Literal ID="portionLit" runat="server"   Text=<%# Eval("portions")    +"<br />" %>       Visible="false"     />
-            <asp:Literal ID="categoryLit" runat="server"  Text=<%# Eval("category")    +"<br />" %>      Visible="false"     />
-            <asp:Literal ID="cuisineLit" runat="server"   Text=<%# Eval("cuisine")     +"<br />" %>       Visible="false"     />
-            <asp:Literal ID="descLit" runat="server"      Text=<%# Eval("description") +"<br />" %>   Visible="false"     />
+            <asp:Literal ID="byLit" runat="server"        Text=<%# "<br />Sumbitted By: " + Eval("fromName")    +"<br />" %>        Visible="false"     />
+            <asp:Literal ID="timeLit" runat="server"      Text=<%# "Cooking time: " +  Eval("cookingTime") +"<br />" %>   Visible="false"     />
+            <asp:Literal ID="portionLit" runat="server"   Text=<%# "For " +  Eval("portions") + " persons"   +"<br />" %>       Visible="false"     />
+            <asp:Literal ID="categoryLit" runat="server"  Text=<%# "Category: " +  Eval("category")    +"<br />" %>      Visible="false"     />
+            <asp:Literal ID="cuisineLit" runat="server"   Text=<%#  "Cuisine: " + Eval("cuisine")     +"<br />" %>       Visible="false"     />
+            <asp:Literal ID="descLit" runat="server"      Text=<%# "Instructions: <br/>" +  Eval("description") +"<br />" %>   Visible="false"     />
             
             <%--ctrls encloded inside Templates cannot be accessed by C# code. Template itself can fire event then we compare 'CommandName' att of ctrl to find which particular item, out of the whole list, fired. 'CommandArgument' is sent to event handler as arg--%>
             <%--if there r more btns then ea will have unique CommandName & will be ID using that--%>
--- recipes.aspx.cs
+++ recipes.aspx.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
+using System.Diagnostics;
+using System.Net;
 //using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//using System.Windows.Forms;//causing ambiguity bw WebControls.Controls & Forms.Controls. So used full statement where needed below
 
 public partial class recipes : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-       if(!IsPostBack) populateRecipes();//without this POSTBACK chk, populating of DataList's Literals gave ERROR. OSme validation breach!!!
+        if (!IsPostBack) populateRecipes();//without this POSTBACK chk, populating of DataList's Literals gave ERROR. OSme validation breach!!!
     }
 
     private void populateRecipes()
@@ -43,21 +46,13 @@ public partial class recipes : System.Web.UI.Page
         }
     }
 
-
+    
 
 
     //In design-mode, select dataList & in 'properties' doubleClick ItenCommand to generate following event handler
     //this handler is called by onItemCommand="" q time one of items in DataList fires an event
     protected void displayRecipes_ItemCommand(object source, DataListCommandEventArgs e)
     {
-        
-
-        
-
-
-
-        
-
         //if the btn e Commandname 'showMore' fired then do this
         if (e.CommandName == "showMore")
         {
@@ -74,33 +69,38 @@ public partial class recipes : System.Web.UI.Page
                             c2.Visible = false;
                         }
                     }
-                }
+                } 
+                //keep focus on the btn clicked
+            ((Button)e.Item.FindControl("showMore")).Focus();
             }
+
+            
+
+
+
+
 
             //if recipe-item clicked was NOT already visible THEN make it visible aft making ALL invisible
             else
             {
-
-            //===========================================================================
-            //Make all Literal ctrls invisible, before making one recipe details visible
-            foreach (Control c1 in displayRecipes.Items)
-            {
-                foreach (Control c2 in c1.Controls)
+                foreach (Control c1 in displayRecipes.Items)
                 {
-                    if (c2 is Literal)
+                    foreach (Control c2 in c1.Controls)
                     {
-                        c2.Visible = false;
+                        if (c2 is Literal)
+                        {
+                            c2.Visible = false;
+                        }
                     }
                 }
-            }
-            //===========================================================================
-            //Literal li1 = (Literal)e.Item.FindControl("byLit"); 
-            //li1.Text = "Submitted By: " + e.CommandArgument;
-            //above 2 stats reduced below:
-            //
-            //((Literal)e.Item.FindControl("byLit")).Text = "Submitted By: " + e.CommandArgument + "<br />";
+                //===========================================================================
+                //Literal li1 = (Literal)e.Item.FindControl("byLit"); 
+                //li1.Text = "Submitted By: " + e.CommandArgument;
+                //above 2 stats reduced below:
+                //
+                //((Literal)e.Item.FindControl("byLit")).Text = "Submitted By: " + e.CommandArgument + "<br />";
 
-            
+
                 //make all pre-populated asp:Literals visible
                 ((Literal)e.Item.FindControl("byLit")).Visible = true;
                 ((Literal)e.Item.FindControl("timeLit")).Visible = true;
@@ -109,11 +109,48 @@ public partial class recipes : System.Web.UI.Page
                 ((Literal)e.Item.FindControl("cuisineLit")).Visible = true;
                 ((Literal)e.Item.FindControl("descLit")).Visible = true;
                 //===========================================================================
+                ((Button)e.Item.FindControl("showMore")).Focus();
             }
-
-
         }
+    }
 
-      
+    protected void openDbFile_Click(object sender, EventArgs e)
+    {
+        ////using System.Diagnostics;
+        ////open file or folder
+        //String absPath = Server.MapPath("cookbook.mdb");//get absolute path in server's folders
+        ////@ ignores the escape character backslash//Process.Start(@"C:\Users\Zoya\Google Drive\_Semester-3\_comp229-web_Primala\_Assignments\aspAssign1");
+        //Process.Start(absPath);
+
+        //============================================================
+
+        //
+        using (var client = new WebClient())
+        {
+            client.DownloadFile("http://studentweb.cencol.ca/shafiqu/aspAssign1/Default.aspx", @"C:\Users\Zoya\Desktop\downLoadedFile.aspx");
+            //System.Windows.Forms.MessageBox.Show("File Downloaded Successfully");//using System.Windows.Forms; giving err so put whole statement here.
+
+            //System.Windows.Forms.MessageBox.Show( //overload used to keep pop-up window atop
+            //    "File Downloaded Successfully",
+            //    "It is a Heading",
+            //    System.Windows.Forms.MessageBoxButtons.OK,
+            //    System.Windows.Forms.MessageBoxIcon.Warning,
+            //    System.Windows.Forms.MessageBoxDefaultButton.Button1,
+            //    System.Windows.Forms.MessageBoxOptions.DefaultDesktopOnly//this line keeps pop-up on top
+            //    );
+
+
+            System.Windows.Forms.MessageBox.Show( //a 3rd way. Use OL that needs a window-obj as 1st arg
+                new System.Windows.Forms.Form { TopMost = true },
+                "File Downloaded Successfullly",
+                "Some Heading"/*,
+                System.Windows.Forms.MessageBoxButtons.OK,
+                System.Windows.Forms.MessageBoxIcon.Exclamation*/
+                );
+
+
+            //Response.Write("Download Successful");//this line runs ONLY if above line was successful
+            //Response.End();//erminates the App
+        }
     }
 }
