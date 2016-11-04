39ea23f2781e57dcc3642eb117c4e938fe694a93
300621334 <shafiqu@my.centennialcollege.ca>
2016-11-04 00:05:41 -0400

    Changed search result display from GridView to DataList

--- search.aspx
+++ search.aspx
@@ -25,8 +25,39 @@
 
     <asp:Button OnClick="searchBtn_Click" ID="searchBtn" runat="server" Text="Search" />
     <br /><br />
-
+<%--================================================================================--%>
+    
+    
     <asp:GridView ID="searchGridView" runat="server"></asp:GridView>
+
+
+<%--================================================================================--%>
+    <asp:DataList ID="displayRecipes" runat="server" OnItemCommand="displayRecipes_ItemCommand">
+        
+        <ItemTemplate>
+            <%# "Recipe:" + Eval("recipeName") %>
+
+            <%--EnableViewState="false" inside Literal would erase itself & showDetal btn doesn't show anything--%>
+            <asp:Literal ID="byLit" runat="server"        Text=<%# "<br />Sumbitted By: " + Eval("fromName")    +"<br />" %>        Visible="false"     />
+            <asp:Literal ID="timeLit" runat="server"      Text=<%# "Cooking time: " +  Eval("cookingTime") +"<br />" %>   Visible="false"     />
+            <asp:Literal ID="portionLit" runat="server"   Text=<%# "For " +  Eval("portions") + " persons"   +"<br />" %>       Visible="false"     />
+            <asp:Literal ID="categoryLit" runat="server"  Text=<%# "Category: " +  Eval("category")    +"<br />" %>      Visible="false"     />
+            <asp:Literal ID="cuisineLit" runat="server"   Text=<%#  "Cuisine: " + Eval("cuisine")     +"<br />" %>       Visible="false"     />
+            <asp:Literal ID="descLit" runat="server"      Text=<%# "Instructions: <br/>" +  Eval("description") +"<br />" %>   Visible="false"     />
+            
+            <%--ctrls encloded inside Templates cannot be accessed by C# code. Template itself can fire event then we compare 'CommandName' att of ctrl to find which particular item, out of the whole list, fired. 'CommandArgument' is sent to event handler as arg--%>
+            <%--if there r more btns then ea will have unique CommandName & will be ID using that--%>
+            <asp:Button ID="showMore" runat="server" Text=<%# "Details of "+ Eval("recipeName") %> CommandName="showMore" CommandArgument=<%# Eval("recipeName") %> />
+            
+
+        </ItemTemplate>
+
+        
+        <SeparatorTemplate>
+            <hr />
+        </SeparatorTemplate>
+
+    </asp:DataList>
 
 
 
--- search.aspx.cs
+++ search.aspx.cs
@@ -56,8 +56,74 @@ public partial class search : System.Web.UI.Page
 
         reader = query.ExecuteReader();//returns a reader
 
-        searchGridView.DataSource = reader;
-        searchGridView.DataBind();
+        //searchGridView.DataSource = reader;
+        //searchGridView.DataBind();
+        displayRecipes.DataSource = reader;
+        displayRecipes.DataBind();
 
+    }
+
+
+    protected void displayRecipes_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        //if the btn e Commandname 'showMore' fired then do this
+        if (e.CommandName == "showMore")
+        {
+            //if details already displayed for the recipe-item that fired & we clicked btn for same again THEN just make everything invisible.
+            if (((Literal)e.Item.FindControl("byLit")).Visible == true)
+            {
+                //Make all Literal ctrls invisible, before making one recipe details visible
+                foreach (Control c1 in displayRecipes.Items)
+                {
+                    foreach (Control c2 in c1.Controls)
+                    {
+                        if (c2 is Literal)
+                        {
+                            c2.Visible = false;
+                        }
+                    }
+                }
+            //keep focus on the btn clicked. Extract ctrl invisible to C# like this
+            ((Button)e.Item.FindControl("showMore")).Focus();
+            }
+
+
+
+
+
+
+
+            //if recipe-item clicked was NOT already visible THEN make it visible aft making ALL invisible
+            else
+            {
+                foreach (Control c1 in displayRecipes.Items)
+                {
+                    foreach (Control c2 in c1.Controls)
+                    {
+                        if (c2 is Literal)
+                        {
+                            c2.Visible = false;
+                        }
+                    }
+                }
+                //===========================================================================
+                //Literal li1 = (Literal)e.Item.FindControl("byLit"); 
+                //li1.Text = "Submitted By: " + e.CommandArgument;
+                //above 2 stats reduced below:
+                //
+                //((Literal)e.Item.FindControl("byLit")).Text = "Submitted By: " + e.CommandArgument + "<br />";
+
+
+                //make all pre-populated asp:Literals visible
+                ((Literal)e.Item.FindControl("byLit")).Visible = true;
+                ((Literal)e.Item.FindControl("timeLit")).Visible = true;
+                ((Literal)e.Item.FindControl("portionLit")).Visible = true;
+                ((Literal)e.Item.FindControl("categoryLit")).Visible = true;
+                ((Literal)e.Item.FindControl("cuisineLit")).Visible = true;
+                ((Literal)e.Item.FindControl("descLit")).Visible = true;
+                //===========================================================================
+                ((Button)e.Item.FindControl("showMore")).Focus();
+            }
+        }
     }
 }
